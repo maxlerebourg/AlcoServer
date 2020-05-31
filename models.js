0a063e4de1875f0901366edf27c2ab68d71296be
maxlerebourg <maxlerebourg@gmail.com>
2020-05-31 18:46:31 +0200

    update

--- models.js
+++ models.js
@@ -2,7 +2,7 @@ import Sequelize from 'sequelize';
 import config from './config.js';
 import { users, categories, comments, games } from './utils/db.js';
 
-const sequelize = new Sequelize(`postgres://postgres:root@127.0.0.1:5432/${config.db}`);
+const sequelize = new Sequelize(`postgres://${config.userDB}:${config.mdp}@127.0.0.1:5432/${config.db}`);
 sequelize.authenticate();
 
 const Op = Sequelize.Op;
