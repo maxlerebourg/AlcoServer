034a18ec623fafed89ea30c747c2e18a681c71b5
maxlerebourg <maxlerebourg@gmail.com>
2020-06-16 19:33:13 +0200

    update

--- models.js
+++ models.js
@@ -110,73 +110,73 @@ Category.hasMany(Game);
 Comment.belongsTo(User);
 Comment.belongsTo(Game);
 
-(async () => {
-	await User.sync({ force: true });
-
-	await Category.sync({ force: true });
-
-	await Game.sync({ force: true });
-
-	await Comment.sync({ force: true });
-
-	await sequelize.query(`
-		create or replace function update_games_comments()
-		returns trigger as $$
-		begin
-			update games set
-			"commentsCount" = "c"."comments_count",
-			"commentsRate" = "c"."comments_rate"
-			from (select "comments"."gameId", count("comments"."rate") as comments_count, avg("comments"."rate") as comments_rate from comments where "comments"."gameId"=new."gameId" group by "comments"."gameId") as "c"
-			where "c"."gameId" = "games"."id";
-			return new;
-		end;
-		$$ language 'plpgsql';
-	`);
-	await sequelize.query(`
-		create trigger trigger_update_games_comment
-		after insert or update on comments
-		for each row
-		execute procedure update_games_comments();
-	`);
-	const usersObject = new Array(categories.length);
-	for (let i = 0; i < users.length; i += 1) {
-		usersObject[i] = await User.create({
-			...users[i],
-			id: undefined,
-		}, { raw: true });
-	}
-
-	const categoriesObject = new Array(categories.length);
-	for (let i = 0; i < categories.length; i += 1) {
-		categoriesObject[i] = await Category.create({
-			...categories[i],
-			id: undefined,
-		}, { raw: true });
-	}
-
-	const gamesObject = new Array(games.length);
-	for (let i = 0; i < games.length; i += 1) {
-		const { visible } = games[i];
-		gamesObject[i] = await Game.create({
-			...games[i],
-			id: undefined,
-			visible: undefined,
-			status: (visible === '1' && '200') || (visible === '0' && '201') || '410',
-			userId: usersObject[+games[i].userId - 1].id,
-			categoryId: categoriesObject[+games[i].categoryId - 1].id,
-		}, { raw: true });
-	}
-
-	const commentsObject = new Array(comments.length);
-	for (let i = 0; i < comments.length; i += 1) {
-		commentsObject[i] = await Comment.create({
-			...comments[i],
-			id: undefined,
-			userId: usersObject[+comments[i].userId - 1].id,
-			gameId: gamesObject[+comments[i].gameId - 1].id,
-		});
-	}
-})();
+// (async () => {
+// 	await User.sync({ force: true });
+//
+// 	await Category.sync({ force: true });
+//
+// 	await Game.sync({ force: true });
+//
+// 	await Comment.sync({ force: true });
+//
+// 	await sequelize.query(`
+// 		create or replace function update_games_comments()
+// 		returns trigger as $$
+// 		begin
+// 			update games set
+// 			"commentsCount" = "c"."comments_count",
+// 			"commentsRate" = "c"."comments_rate"
+// 			from (select "comments"."gameId", count("comments"."rate") as comments_count, avg("comments"."rate") as comments_rate from comments where "comments"."gameId"=new."gameId" group by "comments"."gameId") as "c"
+// 			where "c"."gameId" = "games"."id";
+// 			return new;
+// 		end;
+// 		$$ language 'plpgsql';
+// 	`);
+// 	await sequelize.query(`
+// 		create trigger trigger_update_games_comment
+// 		after insert or update on comments
+// 		for each row
+// 		execute procedure update_games_comments();
+// 	`);
+// 	const usersObject = new Array(categories.length);
+// 	for (let i = 0; i < users.length; i += 1) {
+// 		usersObject[i] = await User.create({
+// 			...users[i],
+// 			id: undefined,
+// 		}, { raw: true });
+// 	}
+//
+// 	const categoriesObject = new Array(categories.length);
+// 	for (let i = 0; i < categories.length; i += 1) {
+// 		categoriesObject[i] = await Category.create({
+// 			...categories[i],
+// 			id: undefined,
+// 		}, { raw: true });
+// 	}
+//
+// 	const gamesObject = new Array(games.length);
+// 	for (let i = 0; i < games.length; i += 1) {
+// 		const { visible } = games[i];
+// 		gamesObject[i] = await Game.create({
+// 			...games[i],
+// 			id: undefined,
+// 			visible: undefined,
+// 			status: (visible === '1' && '200') || (visible === '0' && '201') || '410',
+// 			userId: usersObject[+games[i].userId - 1].id,
+// 			categoryId: categoriesObject[+games[i].categoryId - 1].id,
+// 		}, { raw: true });
+// 	}
+//
+// 	const commentsObject = new Array(comments.length);
+// 	for (let i = 0; i < comments.length; i += 1) {
+// 		commentsObject[i] = await Comment.create({
+// 			...comments[i],
+// 			id: undefined,
+// 			userId: usersObject[+comments[i].userId - 1].id,
+// 			gameId: gamesObject[+comments[i].gameId - 1].id,
+// 		});
+// 	}
+// })();
 
 export { Op, User, Category, Game, Comment, sequelize };
 
